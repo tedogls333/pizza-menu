68304d904b0b0c326673b50720affd41acc52373
tedogls333 <tedogls3@gmail.com>
2024-02-24 11:47:57 +0100

    new styles

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
@@ -47,7 +48,43 @@ const pizzaData = [
 ];
 
 function App() {
-  return <Pizza />;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer>{new Date().toLocaleTimeString()} we're currently open!</footer>
+  );
 }
 
 function Pizza() {
